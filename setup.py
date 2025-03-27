31abe584cd14f993f085ae6b784a427d418ea72a
huanjun.zhao <huanjun.zhao@ucalgary.ca>
2025-03-26 23:01:50 -0600

    Change setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(name='unitree_sdk2py',
-      version='1.0.1',
-      author='UnitreeRobotics',
-      author_email='unitree@unitree.com',
+setup(name='Robot_Haptics UofC',
+      version='0.0.1',
+      author='Huanjun Zhao',
+      author_email='huanjun.zhao@ucalgary.ca',
       long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
       license="BSD-3-Clause",
       packages=find_packages(include=['unitree_sdk2py','unitree_sdk2py.*']),
-      description='Unitree robot sdk version 2 for python',
+      description='Unitree robot sdk version 2 for python, modified by Huanjun Zhao, University of Calgary. Copyright (c) 2016-2024 HangZhou YuShu TECHNOLOGY CO.,LTD. ("Unitree Robotics") All rights reserved.',
       project_urls={
-            "Source Code": "https://github.com/unitreerobotics/unitree_sdk2_python",
+            "Source Code": "https://github.com/HuanjunZhao/unitree_sdk2_python",
       },
       python_requires='>=3.8',
       install_requires=[
